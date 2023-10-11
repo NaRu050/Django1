3495195b0d759a97e785f4126505dcbf6bc8f79e
NaRu050 <147577613+NaRu050@users.noreply.github.com>
2023-10-11 10:13:44 +0530

    Update settings.py Allowed all the hosts

--- Djangoproject1/settings.py
+++ Djangoproject1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(2xv2i)li+@p61fg(okmn*x-+hsg(#_q*!qle@%+3pn1p2q-w$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
